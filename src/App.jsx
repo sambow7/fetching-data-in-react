207462abdbc8b3f5e6dce3661759b4479f31ddea
Sam Bowen <samhbowen@gmail.com>
2025-02-15 10:20:39 -0800

    add FetchCityName func,

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import { useState, useEffect } from 'react';
 
 const App = () => {
   const [weather, setWeather] = useState({});
+  const [user, setUser] = useState(null);
+
   const fetchData = async (city) => {
     const data = await weatherService.show(city);
     const newWeatherState = {
@@ -16,35 +18,35 @@ const App = () => {
     };
     setWeather(newWeatherState);
   };
-  // The following log should be outside of the fetchData function
   console.log('State:', weather);
 
   useEffect(() => {
-
-    // Define a fetch function:
-    const fetchDefaultData = async () => {
-      const data = await weatherService.show('London');
-      const newWeatherState = {
-        location: data.location.name,
-        temperature: data.current.temp_f,
-        condition: data.current.condition.text,
-      };
-      setWeather(newWeatherState);
+    const fetchCityName = async (latitude, longitude) => {
+      const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`);
+      const data = await response.json();
+      return data.postcode
     };
 
-    // Call the fetch function when the page loads:
-    fetchDefaultData();
-
-
-  }, []); // An empty dependency array means this runs once after the initial render
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      const city = await fetchCityName(latitude, longitude);
+      const data = await weatherService.show(city);
+      const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+      };
+      setWeather(newWeatherState);
+    });
+  }, []);
 
   return (
     <main>
       <h1>Weather API</h1>
-      <WeatherSearch fetchData={fetchData} />
-      <WeatherDetails weather={weather} />
+      <WeatherSearch fetchData={fetchData}/>
+      <WeatherDetails weather={weather}/>
     </main>
   );
 };
 
-export default App;
+export default App
